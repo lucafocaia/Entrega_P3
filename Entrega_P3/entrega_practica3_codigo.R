225a29c4066f54510a60c6e844c0c2858c488719
lucafocaia <lucafocaia.12@gmail.com>
2023-10-23 11:52:56 -0300

    23/10/23

--- Entrega_P3/entrega_practica3_codigo.R
+++ Entrega_P3/entrega_practica3_codigo.R
@@ -46,39 +46,35 @@ datos.mendoza[datos.mendoza==9999.9] = NA
 
 
 #Hago una lista para cada estacion
-lista.azul = list("AZUL", info[1,2], info[1,3], info[1,4], datos.azul[[1]][1],
-                  datos.azul[,2],datos.azul[,3], datos.azul[,4], datos.azul[,5])
-lista.aeroparque = list("AEROPARQUE", info[2,2], info[2,3], info[2,4],
-                        datos.aeroparque[[1]][1], datos.aeroparque[,2], 
-                        datos.aeroparque[,3], datos.aeroparque[,4], 
-                        datos.aeroparque[,5])
-lista.catamarca = list("CATAMARCA", info[3,2], info[3,3], info[3,4],
-                      datos.catamarca[[1]][1], datos.catamarca[,2],
-                      datos.catamarca[,3], datos.catamarca[,4],
-                      datos.catamarca[,5])
-lista.chilecito = list("CHILECITO", info[4,2], info[4,3], info[4,4],
-                       datos.chilecito[[1]][1], datos.chilecito[,2], 
-                       datos.chilecito[,3], datos.chilecito[,4],
-                       datos.chilecito[,5])
-lista.iguazu = list("IGUAZU", info[5,2], info[5,3], info[5,4],
-                    datos.iguazu[[1]][1], datos.iguazu[,2], datos.iguazu[,3],
-                    datos.iguazu[,4], datos.iguazu[,5])
-lista.mendoza = list("MENDOZA", info[6,2], info[6,3], info[6,4],
-                     datos.mendoza[[1]][1], datos.mendoza[,2],
-                     datos.mendoza[,3], datos.mendoza[,4], datos.mendoza[,5])
+lista.azul = list("Nombre"="AZUL", "Lat"=info[1,2], "Lon"=info[1,3],
+                  "Altura"=info[1,4], "Cod"=datos.azul[[1]][1],
+                  "Dia"=datos.azul[,2], "Temp"=datos.azul[,3],
+                  "Temp_ro"=datos.azul[,4], "Presion"=datos.azul[,5])
+lista.aeroparque = list("Nombre"="AEROPARQUE", "Lat"=info[2,2], "Lon"=info[2,3],
+                        "Altura"=info[2,4], "Cod"=datos.aeroparque[[1]][1],
+                        "Dia"=datos.aeroparque[,2], "Temp"=datos.aeroparque[,3],
+                        "Temp_ro"=datos.aeroparque[,4], "Presion"=datos.aeroparque[,5])
+lista.catamarca = list("Nombre"="CATAMARCA", "Lat"=info[3,2], "Lon"=info[3,3],
+                       "Altura"=info[3,4], "Cod"=datos.catamarca[[1]][1],
+                       "Dia"=datos.catamarca[,2], "Temp"=datos.catamarca[,3],
+                       "Temp_ro"=datos.catamarca[,4], "Presion"=datos.catamarca[,5])
+lista.chilecito = list("Nombre"="CHILECITO", "Lat"=info[4,2], "Lon"=info[4,3],
+                       "Altura"=info[4,4], "Cod"=datos.chilecito[[1]][1],
+                       "Dia"=datos.chilecito[,2], "Temp"=datos.chilecito[,3],
+                       "Temp_ro"=datos.chilecito[,4], "Presion"=datos.chilecito[,5])
+lista.iguazu = list("Nombre"="IGUAZU", "Lat"=info[5,2], "Lon"=info[5,3],
+                    "Altura"=info[5,4], "Cod"=datos.iguazu[[1]][1],
+                    "Dia"=datos.iguazu[,2], "Temp"=datos.iguazu[,3],
+                    "Temp_ro"=datos.iguazu[,4], "Presion"=datos.iguazu[,5])
+lista.mendoza = list("Nombre"="MENDOZA", "Lat"=info[6,2], "Lon"=info[6,3],
+                     "Altura"=info[6,4], "Cod"=datos.mendoza[[1]][1],
+                     "Dia"=datos.mendoza[,2], "Temp"=datos.mendoza[,3],
+                     "Temp_ro"=datos.mendoza[,4], "Presion"=datos.mendoza[,5])
 
 
 #Armo el array de listas
-estaciones = array(list(),dim=c(6,9))
-colnames(estaciones) = c("Nombre", "Latitud", "Longitud", "Altura", "Cod.id",
-                         "Dia","Temp", "Temp.rocio", "Presion")
-estaciones[1,] = lista.azul
-estaciones[2,] = lista.aeroparque
-estaciones[3,] = lista.catamarca
-estaciones[4,] = lista.chilecito
-estaciones[5,] = lista.iguazu
-estaciones[6,] = lista.mendoza
-
+estaciones = list(lista.azul, lista.aeroparque, lista.catamarca, lista.chilecito,
+                  lista.iguazu, lista.mendoza)
 
 
 ### 2 ###
@@ -87,33 +83,34 @@ estaciones[6,] = lista.mendoza
 #Funcion que da el resumen de varias series de datos
 resumen = function(x) { #Arme un array, en vez de un data.frame, me parecio 
                          #mas comodo
-  informacion = array(list(),dim=c(nrow(x),16)) 
-  colnames(informacion) = c("Estacion","Cantidad de datos","Temperatura media",
+ informacion = array(list(),dim=c(length(x),16))
+ colnames(informacion) = c("Estacion","Cantidad de datos","Temperatura media",
                             "Temperatura de rocio media","Presion media",
                             "Desvio de la temperatura", "Desvio de la 
                             temperatura de rocio","Desvio de la presion",
                             "Temperatura maxima","Temperatura de rocio maxima",
                             "Presion maxima","Temperatura minima",
                             "Temperatura de rocio minima","Presion minima",
-                            "Inicio","Final") 
- for (i in 1:nrow(x)) {
-    estacion = x[[i,"Nombre"]]
-    cantdatos = length(x[[i,"Dia"]])
-    mediatemp = mean(x[[i,"Temp"]], na.rm = T)
-    mediarocio = mean(x[[i,"Temp.rocio"]], na.rm = T)
-    mediapresion = mean(x[[i,"Presion"]], na.rm = T)
-    dstemp = sd(x[[i,"Temp"]], na.rm = T)
-    dsrocio = sd(x[[i,"Temp.rocio"]], na.rm = T)
-    dspresion = sd(x[[i,"Presion"]], na.rm = T)
-    maxtemp = max(x[[i,"Temp"]], na.rm = T)
-    maxrocio = max(x[[i,"Temp.rocio"]], na.rm = T)
-    maxpresion = max(x[[i,"Presion"]], na.rm = T)
-    mintemp = min(x[[i,"Temp"]], na.rm = T)
-    minrocio = min(x[[i,"Temp.rocio"]], na.rm = T)
-    minpresion = min(x[[i,"Presion"]], na.rm = T)
-    inicio = x[[i,"Dia"]][1]
-    final = x[[i,"Dia"]][length(estaciones[[i,"Dia"]])]
-    informacion[i,] = c(estacion, cantdatos, mediatemp, mediarocio,
+                            "Inicio","Final")
+ for (i in 1:length(x)) {
+    estacion = x[[i]]
+    nombre = estacion$Nombre
+    cantdatos = length(estacion$Dia)
+    mediatemp = round(mean(estacion$Temp, na.rm = T),2)
+    mediarocio = round(mean(estacion$Temp_ro, na.rm = T),2)
+    mediapresion = round(mean(estacion$Presion, na.rm = T),2)
+    dstemp = round(sd(estacion$Temp, na.rm = T),2)
+    dsrocio = round(sd(estacion$Temp_ro, na.rm = T),2)
+    dspresion = round(sd(estacion$Presion, na.rm = T),2)
+    maxtemp = round(max(estacion$Temp, na.rm = T),2)
+    maxrocio = round(max(estacion$Temp_ro, na.rm = T),2)
+    maxpresion = round(max(estacion$Presion, na.rm = T),2)
+    mintemp = round(min(estacion$Temp, na.rm = T),2)
+    minrocio = round(min(estacion$Temp_ro, na.rm = T),2)
+    minpresion = round(min(estacion$Presion, na.rm = T),2)
+    inicio = estacion$Dia[1]
+    final = estacion$Dia[length(estacion$Dia)]
+    informacion[i,] = c(nombre, cantdatos, mediatemp, mediarocio,
                         mediapresion, dstemp, dsrocio, dspresion, maxtemp,
                         maxrocio, maxpresion, mintemp, minrocio, minpresion,
                         inicio, final)
@@ -127,29 +124,20 @@ resumen.estaciones = resumen(estaciones) #Salta un Warning porque Chilecito
 
 
 #II
-localizacion_de_estaciones = function(latmin,latmax,lonmin,lonmax) {
-  if (-36.8>=latmin & -36.8<=latmax & -59.9>=lonmin & -59.9<=lonmax) {
-    print("La estacion de Azul se encuentra en esta region")
-    if (-34.6>=latmin & -34.6<=latmax & -58.4>=lonmin & -58.4<=lonmax) {
-      print("La estacion de Aeroparque se encuentra en esta region")
-    } 
-    if (-28.6>=latmin & -28.6<=latmax & -65.8>=lonmin & -65.8<=lonmax) {
-      print("La estacion de Catamarca se encuentra en esta region")
-    } 
-    if (-29.2>=latmin & -29.2<=latmax & -67.4>=lonmin & -67.4<=lonmax) {
-      print("La estacion de Chilecito se encuentra en esta region")
+localizacion_de_estaciones = function(x,latmin,latmax,lonmin,lonmax) {
+  for (i in 1:length(x)) {
+    estacion = x[[i]]
+    if (as.numeric(estacion$Lat)>=latmin & as.numeric(estacion$Lat)<=latmax & 
+        as.numeric(estacion$Lon)>=lonmin & as.numeric(estacion$Lon)<=lonmax) {
+      print(paste("La estacion",estacion$Nombre,"se encuentra en esta region"))
+    } else {
+      print(paste("La estacion",estacion$Nombre,"no se encuentra en esta region"))
     }
-    if (-25.7>=latmin & -25.7<=latmax & -54.5>=lonmin & -54.5<=lonmax) {
-      print("La estacion de Iguazu se encuentra en esta region")
-    } 
-    if (-32.5>=latmin & -32.5<=latmax & -68.5>=lonmin & -68.5<=lonmax) {
-      print("La estacion de Mendoza se encuentra en esta region")
-    }
-  } else {print("No hay estaciones cercanas")}
+  }
 }
 
-localizacion_de_estaciones(-70,-20,-70,-20)
-localizacion_de_estaciones(30,60,30,60)
+localizacion_de_estaciones(estaciones,-70,-20,-70,-20)
+localizacion_de_estaciones(estaciones,30,60,30,60)
 
 
 #III
@@ -158,5 +146,8 @@ guardar.archivo = function(x,nombre_archivo) {
 }
 
 guardar.archivo(estaciones,"Datos_Estaciones.Rdata")
+
+
+
 
 
